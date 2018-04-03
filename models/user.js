689a377a14def510eb6a4b7887b60591ec887cbb
Trevor Harm <trevorharm@users.noreply.github.com>
2018-04-02 20:10:44 -0400

    userUpdated

--- models/user.js
+++ models/user.js
@@ -1,1 +1,8 @@
-dsafadssa
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+      email: DataTypes.STRING,
+      password: DataTypes.STRING
+    });
+    return User;
+  };
+  
