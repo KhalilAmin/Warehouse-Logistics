fadf17adf505bc5f0c68e06bdbf4c09f2d65e881
pawhalen <pgwhal@gmail.com>
2018-04-05 21:30:26 -0400

    getting the canned table to display

---
+++ db/temp_seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO Companies (Name) VALUES ("SodaRama");
+INSERT INTO Companies (Name) VALUES ("SodaBama");
+
+INSERT INTO Sites (site_name, line_1, line_2, city, state, zip, country, CompanyCompanyId) 
+VALUES ("Site1", "SomeStreet", "Some Apt", "Some City", "NC", "28203", "USA", 1);
+
+INSERT INTO Sites (site_name, line_1, line_2, city, state, zip, country, CompanyCompanyId) 
+VALUES ("Site2", "SomeStreet2", "Some Apt2", "Some City2", "NC", "28203", "USA", 2);
+
+
+INSERT INTO Sites (Name) VALUES ("SodaBama");
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Royal with Cheese", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bigger Mac", FALSE);
--- routes/canned-api-routes.js
+++ routes/canned-api-routes.js
@@ -1,45 +1,21 @@
 var db = require("../models");
 
 module.exports = function(app) {
-//   app.get("/api/authors", function(req, res) {
-//     // Here we add an "include" property to our options in our findAll query
-//     // We set the value to an array of the models we want to include in a left outer join
-//     // In this case, just db.Post
-//     db.Author.findAll({
-//       include: [db.Post]
-//     }).then(function(dbAuthor) {
-//       res.json(dbAuthor);
-//     });
-//   });
 
-//   app.get("/api/authors/:id", function(req, res) {
-//     // Here we add an "include" property to our options in our findOne query
-//     // We set the value to an array of the models we want to include in a left outer join
-//     // In this case, just db.Post
-//     db.Author.findOne({
-//       where: {
-//         id: req.params.id
-//       },
-//       include: [db.Post]
-//     }).then(function(dbAuthor) {
-//       res.json(dbAuthor);
-//     });
-//   });
+  app.post("/api/queryData", function(req, res) {
+    db.Site.findAll({
+      // where: {
+      //   site_name: req.body.siteName
+      // }
+    }).then(function(result) {
+      console.log("Result", result);
+      var columns = Object.keys(result[0].dataValues);
+      var data = {
+        columns: columns,
+        data: result
+      };
 
-//   app.post("/api/authors", function(req, res) {
-//     db.Author.create(req.body).then(function(dbAuthor) {
-//       res.json(dbAuthor);
-//     });
-//   });
-
-//   app.delete("/api/authors/:id", function(req, res) {
-//     db.Author.destroy({
-//       where: {
-//         id: req.params.id
-//       }
-//     }).then(function(dbAuthor) {
-//       res.json(dbAuthor);
-//     });
-//   });
-
+      res.json(data);
+    })
+  });
 };
