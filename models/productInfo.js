96aa1b4abdcb86e70776db6fea231f263434d6a3
Trevor Harm <trevorharm@users.noreply.github.com>
2018-04-07 11:56:14 -0400

    tere

--- models/productInfo.js
+++ models/productInfo.js
@@ -62,10 +62,7 @@ module.exports = function (sequelize, DataTypes) {
                 allowNull: false
             }
         });
-        // Associating Product with warehouse specific data
-        // Product_Info.hasMany(models.Wh_Prod_Data, {
-        
-        // });
+
     };
 
     return Product_Info;
