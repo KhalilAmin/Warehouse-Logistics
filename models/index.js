e25057c6966de1eef0e48e6839f6d72d56eff58d
pawhalen <pgwhal@gmail.com>
2018-04-13 15:21:04 -0400

    first try

--- models/index.js
+++ models/index.js
@@ -4,25 +4,14 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
+var env = process.env.NODE_ENV || "production";
+var config = require(__dirname + "/../config/config.json")[env];
+var db = {};
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-  });
 }
 
 fs
