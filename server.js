088c331d99d18f80e271a917e67400cda27a7f6b
Slickrick88 <rickablake@gmail.com>
2018-04-13 15:51:17 -0400

    update index

--- server.js
+++ server.js
@@ -25,8 +25,6 @@ var db = require("./models");
 
 // Sets up the Express app to handle data parsing
 
-
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
