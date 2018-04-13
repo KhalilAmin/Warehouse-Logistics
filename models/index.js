45b184d627c39b354879b15d0b9b914d3c5d686b
Slickrick88 <rickablake@gmail.com>
2018-04-13 15:56:47 -0400

    update to index

--- models/index.js
+++ models/index.js
@@ -1,14 +1,16 @@
 'use strict';
 
-var fs        = require('fs');
-var path      = require('path');
+var fs = require('fs');
+var path = require('path');
 var Sequelize = require('sequelize');
-var basename  = path.basename(__filename);
-var env       = process.env.JAWSDB_URL || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
+var basename = path.basename(__filename);
+// var env       = process.env.JAWSDB_URL || 'development';
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname + '/../config/config.json')[env];
+var db = {};
 
-if (process.env.JAWSDB_URL) {
+// if (process.env.JAWSDB_URL) {
+if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env.JAWSDB_URL, config);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
